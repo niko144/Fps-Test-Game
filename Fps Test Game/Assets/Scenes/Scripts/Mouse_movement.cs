352a1848ef87bed94afaa6762f6bfa06318b297f
ImMortaLGuruji <74717080+ImMortaLGuruji@users.noreply.github.com>
2020-11-20 17:18:11 +0530

    Movement script completed

---
+++ Fps Test Game/Assets/Scenes/Scripts/Mouse_movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_movement : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    public float xrotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xrotation -= mouseY;
+        xrotation = Mathf.Clamp(xrotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
